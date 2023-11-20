105180049da2109c8574ca13fa844bd40bd430b9
Lawrence Amoafo <rekenburg777@gmail.com>
2023-11-20 18:58:19 +0000

    Change the scope method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :registrations => 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    get 'login', to: "devise/sessions#new"
+  end
+
+  devise_scope :user do
+    get 'signup', to: "devise/registrations#new"
+  end
 end
