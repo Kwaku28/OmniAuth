c4b2e3dacd9a1a5660d9ea5d09552f707ba453bd
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-11 19:57:25 +0000

    Update route with the omniauth callbacks for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => 'registrations' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, :controllers => { :registrations => 'registrations', :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   devise_scope :user do
     get 'login', to: "devise/sessions#new"
