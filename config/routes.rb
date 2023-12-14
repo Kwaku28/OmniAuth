36c7039cbdb0d190c94483f0299a5d8d2817732d
Lawrence Amoafo <rekenburg777@gmail.com>
2023-12-14 12:15:34 +0000

    Add root access to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'registrations', :omniauth_callbacks => 'users/omniauth_callbacks' }
 
+  root 'users#splash'
+
   devise_scope :user do
     get 'login', to: "devise/sessions#new"
   end
